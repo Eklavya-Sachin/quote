cf01e60d0f54b7cf010b9b3a45962cc40b50848d
Sachin Kumar <ojaz07860@gmail.com>
2022-12-02 18:47:47 +0530

    Created home screen UI.

---
+++ lib/app/modules/home/views/components/gradient_button.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class GradientButton extends StatelessWidget {
+  const GradientButton({
+    super.key,
+    required this.buttonText,
+    required this.onTap,
+    required this.buttonGradientColors,
+  });
+
+  final String buttonText;
+  final void Function() onTap;
+  final List<Color> buttonGradientColors;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.only(
+          top: 20,
+          left: 20,
+          right: 20,
+        ),
+        height: 150,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          gradient: LinearGradient(
+            colors: buttonGradientColors,
+          ),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(2, 2),
+              blurRadius: 10,
+            ),
+          ],
+        ),
+        child: Center(
+          child: Text(
+            buttonText,
+            style: const TextStyle(
+              fontSize: 30,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,22 +1,62 @@
-import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
+import 'package:flutter/material.dart';
+import 'components/gradient_button.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+    return SafeArea(
+      child: Scaffold(
+        body: ListView(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 20, left: 20),
+              child: const Text(
+                "Welcome",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black87,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 20),
+              child: const Text(
+                "Choose any one",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black87,
+                ),
+              ),
+            ),
+            GradientButton(
+              buttonText: 'Search Author Quote',
+              onTap: controller.goToAuthorQuoteSearch,
+              buttonGradientColors: const <Color>[
+                Colors.black54,
+                Color.fromARGB(255, 206, 112, 19),
+              ],
+            ),
+            GradientButton(
+              buttonText: 'Search Any Quote',
+              onTap: controller.goToQuotesSearch,
+              buttonGradientColors: const <Color>[
+                Colors.black54,
+                Color.fromARGB(255, 12, 43, 97)
+              ],
+            ),
+            GradientButton(
+              buttonText: 'Favourite Quote',
+              onTap: controller.goToFavouriteQuotes,
+              buttonGradientColors: const <Color>[
+                Colors.black54,
+                Color.fromARGB(255, 222, 45, 104)
+              ],
+            ),
+            const SizedBox(height: 20),
+          ],
         ),
       ),
     );
