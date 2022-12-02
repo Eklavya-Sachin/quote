079cac6ee03fccac9f212bda552c80ddcba34e18
Sachin Kumar <ojaz07860@gmail.com>
2022-12-02 18:59:39 +0530

    Added Navigation only.

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,9 +1,16 @@
 import 'package:get/get.dart';
+import '../../../routes/app_pages.dart';
 
 class HomeController extends GetxController {
-  void goToAuthorQuoteSearch() {}
+  void goToAuthorQuoteSearch() {
+    Get.toNamed(Routes.SEARCH_AUTHOR_QUOTE);
+  }
 
-  void goToQuotesSearch() {}
+  void goToQuotesSearch() {
+    Get.toNamed(Routes.SEARCH_QUOTES);
+  }
 
-  void goToFavouriteQuotes() {}
+  void goToFavouriteQuotes() {
+    Get.toNamed(Routes.FAVOURITE_QUOTES);
+  }
 }
