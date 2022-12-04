9700611d59663d4076eda36eb771a8df796edb29
Sachin Kumar <ojaz07860@gmail.com>
2022-12-04 16:42:49 +0530

    Added instance of Api Service class in get put.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
+import 'app/services/api_service.dart';
 
 void main() {
+  Get.lazyPut<ApiService>(
+    () => ApiService(),
+  );
+
   runApp(
     GetMaterialApp(
-      title: "Application",
+      title: "Quotes",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
       debugShowCheckedModeBanner: false,
